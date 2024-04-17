9ddbbebe8280e47634a86f85b3611f63c8ee66ff
Alexandre <allesalg@gmail.com>
2024-04-17 08:48:29 -0300

    feat: add search getAllProdutcs

--- client/stock-control/src/app/features/dashboard/dashboard-home/dashboard-home.component.ts
+++ client/stock-control/src/app/features/dashboard/dashboard-home/dashboard-home.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { ProductsService } from './../../../services/products/products.service';
+import { Component, OnInit } from '@angular/core';
 import { SharedModule } from "../../../shared/shared.module";
+import { GetAllProductsResponse } from '../../../models/interfaces/products/getAllProductsResponse';
+import { ProductsDataTransferService } from '../../../shared/services/products/products-data-transfer.service';
 
 @Component({
     selector: 'app-dashboard-home',
     templateUrl: './dashboard-home.component.html',
     styleUrl: './dashboard-home.component.scss',
 })
-export class DashboardHomeComponent {
+export class DashboardHomeComponent implements OnInit {
 
+    public productsList: Array<GetAllProductsResponse> = []
+
+    constructor(private productsService: ProductsService, private messageService: MessageService, private productsDataTransferService: ProductsDataTransferService) {}
+
+    ngOnInit(): void {
+        this.getProductsData();
+       
+    }
+
+
+    getProductsData(): void {
+        this.productsService.getAllProducts().subscribe({
+            next: (response) => {
+                if(response.length > 0){
+                    this.productsList = response;
+                    this.productsDataTransferService.setProdutcsDatas(this.productsList)
+
+                }
+            },
+            error: (err) => {
+                console.log(err);
+                this.messageService.add({
+                    severity: 'error',
+                    summary: 'Erro',
+                    detail: 'Erro ao buscar produtos',
+                    life: 2500
+                })
+            }
+        })
+    }
 }
---
+++ client/stock-control/src/app/shared/services/products/products-data-transfer.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, map, take } from 'rxjs';
+import { GetAllProductsResponse } from '../../../models/interfaces/products/getAllProductsResponse';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsDataTransferService {
+
+  public productsDataEmitter$ = new BehaviorSubject<Array<GetAllProductsResponse>  | null>(null)
+    public productsDatas: Array<GetAllProductsResponse> = [];
+
+
+  setProdutcsDatas(products: Array<GetAllProductsResponse>): void {
+    if(products) {
+      this.productsDataEmitter$.next(products)
+    }
+  }
+
+  getProductsData() {
+    this.productsDataEmitter$.pipe(take(1), map((data)=> data?.filter((product)=> product.amount > 0))).subscribe({
+      next: (response)=> {
+        if(response){
+          this.productsDatas = response;
+        }
+      }
+    })
+    return this.productsDatas
+  }
+}
